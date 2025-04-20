1bbefa57cea094930718dab04720f582d25ef50e
fajaramaulana <fajaragusmaulana@gmail.com>
2025-04-20 14:31:43 +0700

    Add CI workflow for backend with Docker image build and unit tests

---
+++ build_push_image_karsajobs.sh
@@ -0,0 +1,11 @@
+set -e
+
+# Build image
+IMAGE_NAME="ghcr.io/fajaramaulana/karsajobs:latest"
+docker build -t ${IMAGE_NAME} .
+
+# Login ke GitHub Container Registry dengan token yang tersimpan di env variable PASSWORD_GITHUB_TOKEN
+echo $PASSWORD_GITHUB_TOKEN | docker login ghcr.io -u fajaramaulana --password-stdin
+
+# Push image
+docker push ${IMAGE_NAME}
