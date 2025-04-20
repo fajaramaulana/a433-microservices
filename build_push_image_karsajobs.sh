0998f04d4efd6eca870fbc8d638276ce04299756
fajaramaulana <fajaragusmaulana@gmail.com>
2025-04-20 14:46:12 +0700

    Comment out Docker login command in build script

--- build_push_image_karsajobs.sh
+++ build_push_image_karsajobs.sh
@@ -5,7 +5,7 @@ IMAGE_NAME="ghcr.io/fajaramaulana/karsajobs:latest"
 docker build -t ${IMAGE_NAME} .
 
 # Login ke GitHub Container Registry dengan token yang tersimpan di env variable PASSWORD_GITHUB_TOKEN
-echo $PASSWORD_GITHUB_TOKEN | docker login ghcr.io -u fajaramaulana --password-stdin
+# echo $PASSWORD_GITHUB_TOKEN | docker login ghcr.io -u fajaramaulana --password-stdin
 
 # Push image
 docker push ${IMAGE_NAME}
